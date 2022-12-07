c361d625079ab8f453963cf62d3d294dd7a655e1
ldhwaddell <ldhwaddell@gmail.com>
2022-12-07 10:02:53 -0400

    update q6 C logic

--- q6.c
+++ q6.c
@@ -4,13 +4,13 @@
 //return truncated output or -1 if error occurred
 int doMath(int a, int b, int c)
 {
-    if (0 == c)
+    if (0 == b)
     {
         return INT_MAX - 1;
     }
     else
     {
-        return ((a * b) / c);
+        return ((a / b) - c);
     }
 }
 
