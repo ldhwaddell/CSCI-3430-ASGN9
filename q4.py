201b7dfb16175e717f72eefb749a0334d5789d47
ldhwaddell <ldhwaddell@gmail.com>
2022-12-07 10:36:01 -0400

    update q4 logic

--- q4.py
+++ q4.py
@@ -3,18 +3,26 @@ import sys
 
 def validate_input():
     """
-    Function to validate the user input and either return 
+    Function to validate the user input and either return
     validated input or raise a value error
     """
     try:
-        sqft = input("Input the size of the driveway in square feet: ")
+        width = input("Input the width of the driveway in feet: ")
+        length = input("Input the length of the driveway in feet: ")
         depth = input("Input the depth of the driveway in inches: ")
 
         try:
-            sqft = int(sqft)
+            width = int(width)
         except ValueError:
             raise ValueError(
-                "[-] Error: Input size must be an integer. Program Exiting"
+                "[-] Error: Input width must be an integer. Program Exiting"
+            )
+
+        try:
+            length = int(length)
+        except ValueError:
+            raise ValueError(
+                "[-] Error: Input length must be an integer. Program Exiting"
             )
 
         try:
@@ -24,9 +32,14 @@ def validate_input():
                 "[-] Error: Input depth must be an integer. Program Exiting"
             )
 
-        if sqft < 1:
+        if width < 1:
             raise ValueError(
-                "[-] Error: Input size must be greater than 0. Program Exiting"
+                "[-] Error: Input width must be greater than 0. Program Exiting"
+            )
+
+        if length < 1:
+            raise ValueError(
+                "[-] Error: Input length must be greater than 0. Program Exiting"
             )
 
         if depth < 1:
@@ -34,15 +47,15 @@ def validate_input():
                 "[-] Error: Input depth must be greater than 0. Program Exiting"
             )
 
-        return (sqft, depth)
+        return (width, length, depth)
 
     except ValueError as err:
         raise err
 
 
-def calculate_price(sqft, depth):
+def calculate_price(width, length, depth):
     """Function to calculate the price based on constraints in q4"""
-    cubic_feet = int(sqft * (depth / 12))
+    cubic_feet = int(width * length * (depth / 12))
     price = 0
 
     if cubic_feet <= 199:
@@ -54,18 +67,18 @@ def calculate_price(sqft, depth):
         price = 1000 + (10 * (cubic_feet - 199))
 
     elif cubic_feet >= 400:
-        print(f"Breakdown: 1000 + 1000 + {20 * (cubic_feet - 399)}")
-        price = 2000 + (20 * (cubic_feet - 399))
+        print(f"Breakdown: 1000 + 2000 + {20 * (cubic_feet - 399)}")
+        price = 3000 + (20 * (cubic_feet - 399))
 
     return price
 
 
 if __name__ == "__main__":
     try:
-        sqft, depth = validate_input()
+        width, length, depth = validate_input()
     except ValueError as ex:
         print(ex)
         sys.exit(1)
     else:
-        price = calculate_price(sqft, depth)
+        price = calculate_price(width, length, depth)
         print(f"${price}")
